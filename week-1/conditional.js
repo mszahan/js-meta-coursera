f2c5e674b6e9f2818b6f8aa487bf4b4b641b78ab
mszahan <mszahan1@gmail.com>
2023-10-21 01:01:08 +0600

    explored the conditionals and switch case in javascript

---
+++ week-1/conditional.js
@@ -0,0 +1,58 @@
+// switch case
+var place = 'first'
+
+switch (place) {
+    case 'first':
+        console.log('Gold');
+        break;
+    case 'second':
+        console.log('silver');
+        break
+    case 'third':
+        console.log('bronze');
+        break
+    default:
+        console.log('No medal');
+}
+
+
+
+// else if
+// var place = 'first';
+
+// if (place == 'first'){
+//     console.log('Gold')
+// } else if (place == 'second'){
+//     console.log('silver')
+// } else if (place == 'third'){
+//     console.log('bronze')
+// } else {
+//     console.log('No medal')
+// }
+
+
+
+
+
+// if statement
+// var result = 30;
+
+// if (result >40 ) {
+//     console.log('you passed the test')
+// } else {
+//     console.log('You did not passed')
+// }
+
+
+
+// var val = false
+
+// if (val == true) {
+//     console.log('You are right')
+// }
+
+// else {
+//     console.log('You are wrong')
+// }
+
+// console.log(typeof(2));
