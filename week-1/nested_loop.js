2f78bafae88cff336463dac3b6c09f98202d3578
mszahan <mszahan1@gmail.com>
2023-10-21 21:32:08 +0600

    used the nested loop

---
+++ week-1/nested_loop.js
@@ -0,0 +1,8 @@
+// multiply table
+
+for (let firstNum = 1; firstNum < 4; firstNum++){
+    console.log('-----The Table of ' + firstNum + ' ---------');
+    for (let secondNum = 1; secondNum <=10; secondNum ++) {
+        console.log(firstNum +' * '+ secondNum + ' = ' + firstNum*secondNum);
+    }
+}
