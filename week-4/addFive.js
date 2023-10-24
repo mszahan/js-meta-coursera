ee23e0f2ac88ce534cbe0c3b09340622fc191719
mszahan <mszahan1@gmail.com>
2023-10-24 19:10:08 +0600

    installed jest and run tested a function addFive

---
+++ week-4/addFive.js
@@ -0,0 +1,22 @@
+function addFive(val){
+    return val + 5;
+};
+
+module.exports = addFive;
+
+
+// instruction for installing jest for testing--------------------
+
+//comands for package.json
+// npm init -y
+
+//installing jest in just this dev folder
+// npm install --save-dev jest
+
+// now replace the test script in package.json to jest
+// "scripts": {
+//     "test": "jest"
+//   },
+
+// now you can rest test lik this.....
+// npm run test
